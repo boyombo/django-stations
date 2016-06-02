14d3cb69812ea26a177735beb6f630c043bd55b2
bayo <bayokrapht@gmail.com>
2016-06-02 23:23:05 +0100

    relaxed unique constraint on uuid

--- stations/drugshare/models.py
+++ stations/drugshare/models.py
@@ -13,7 +13,7 @@ class State(models.Model):
 
 class Pharmacy(models.Model):
     name = models.CharField(max_length=200)
-    uuid = models.CharField(max_length=200, unique=True)
+    uuid = models.CharField(max_length=200)
     phone = models.CharField(max_length=50, blank=True)
     street = models.CharField(max_length=200, blank=True)
     area = models.CharField(max_length=200, blank=True)
